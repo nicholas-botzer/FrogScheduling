48ce3c9707d986253e643109020def3d51dc9052
Nicholas Botzer <nicholas.botzer@gmail.com>
2018-10-19 17:51:28 -0400

    Implementation of initial population

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,10 +1,18 @@
+import random
+
 class GeneticAlgorithm():
 
-    def __init__(self):
-        pass
+    def __init__(self, listOfTasks, numberOfChromosomes, crossoverTechnique):
+        self.listOfChromosomes = []
+        self.numberOfChromosomes = numberOfChromosomes
+        self.initial_population(listOfTasks)
 
-    def initial_population(self):
-        pass
+    def initial_population(self, listOfTasks):
+        
+        for x in range(0, self.numberOfChromosomes):
+            chromosome = Chromosome()
+            shuffeledTasks = random.shuffle(listOfTasks)
+            
 
     def crossover(self):
         pass
@@ -19,8 +27,29 @@ class GeneticAlgorithm():
         pass
 
 
+
+
+
+
+
+
+
 class Chromosome():
 
     def __init__(self):
-        this.
+        self.taskToPriorityDict = {}
+
+    def insert_task(self, task,priority):
+        self.taskToPriorityDict[task] = priority
+
+    def update_task(self, task, priority):
+        if(task in self.taskToPriorityDict):
+            self.taskToPriorityDict[task] = priority
+
+class Task():
+
+    def __init__(self, deadline, arrival_time, wcet):
+        self.deadline = deadline
+        self.arrival_time = arrival_time
+        self.wcet = wcet
 
