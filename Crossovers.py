cc68676451eac99c95e5690033b2f7f9c9641aed
charley-zhang <yzhang46@nd.edu>
2018-10-20 22:12:12 -0400

    Finished OX1 and half of OX2

--- Crossovers.py
+++ Crossovers.py
@@ -6,7 +6,7 @@ Holds static methods implementing different crossover algorithms.
 class Crossovers(object):
 
 ####### HELPER FUNCTIONS #######
-    '''
+    ''' (For OX1)
     Args
     - mini: minimum value in partition range
     - maxi: maximum value in partition range
@@ -15,7 +15,7 @@ class Crossovers(object):
     - list of partition indexes in increasing order
     '''
     @staticmethod #prevents python from passing self instance
-    def generateRPartitions(mini,maxi,n):
+    def generateRandIdxList(mini,maxi,n):
         if n >= maxi-mini:
             return range(mini,maxi+1)
         partitions = []
@@ -24,6 +24,39 @@ class Crossovers(object):
             if ri not in partitions:
                 partitions.append(ri)
         return sorted(partitions)
+    '''
+    Args
+    - taskDict: dictionary that maps tasks to their order index / priority
+    Return
+    - List of tasks where its index indicates its priority
+    '''
+    @staticmethod 
+    def dictToList(taskDict):
+        taskList = len(taskDict)*[None]
+        for k,v in taskDict.items():
+            if v > len(taskDict) - 1:
+                raise ValueError('Dictionary value incorrect! You have'
+                ' index %d in %d number of items' % (v,len(taskDict)))
+            taskList[v] = k
+        if None in taskList:
+            raise ValueError('Dictionary mapping is incorrect!')
+        return taskList
+    '''
+    Args
+    - task: task object 
+    - taskList: list to check if task object is in
+    Return
+    - True if in list, false otherwise
+    '''
+    @staticmethod 
+    def checkTaskInTList(task,taskList):
+        for t in taskList:
+            if t == None:
+                continue
+            if task.name == t.name:
+                return True
+        return False
+
 
 ####### CROSSOVER IMPLEMENTATIONS #######
     '''
@@ -47,41 +80,116 @@ class Crossovers(object):
 
     ### Shuffle
         numTasks = len(taskDict1)
-        newTaskList = numTasks*[None]
-        taskList1, taskList2 = numTasks*[None], numTasks*[None]
-        for (k1,v1), (k2,v2) in zip(taskDict1.items(), taskDict2.items()):
-            taskList1[v1], taskList2[v2] = k1, k2
-        
+        taskList1 = cls.dictToList(taskDict1)
+        taskList2 = cls.dictToList(taskDict2)
+        newTaskList1, newTaskList2 = numTasks*[None], numTasks*[None]
+
         #Get Random Partitions
-        p1,p2 = cls.generateRPartitions(0,numTasks,2)
+        p1,p2 = cls.generateRandIdxList(0,numTasks,2)
         if partition1 == None:
             partition1 = p1
         if partition2 == None:
             partition2 = p2
         #Crossover
         for idx in range(partition1,partition2): #copy elems from p1 to p2-1 
-            newTaskList[idx] = taskList1[idx]
-        tL2idx = nTLidx = partition2
-        while None in newTaskList:
+            newTaskList1[idx] = taskList1[idx] # * * * | l1idx3 l1idx4 | * ...
+            newTaskList2[idx] = taskList2[idx] # * * * | l2idx3 l2idx4 | * ...
+        tL1idx = tL2idx = nTL1idx = nTL2idx = partition2
+        nTL1done, nTL2done = False, False
+        while not (nTL1done and nTL2done):
+            if tL1idx >= numTasks:
+                tL1idx = 0
             if tL2idx >= numTasks:
                 tL2idx = 0
-            if nTLidx >= numTasks:
-                nTLidx = 0
-            if taskList2[tL2idx] not in newTaskList:
-                newTaskList[nTLidx] = taskList2[tL2idx]
-                nTLidx += 1
-            tL2idx += 1 #task list 2 index incremented either way
+            if nTL1idx >= numTasks:
+                nTL1idx = 0
+            if nTL2idx >= numTasks:
+                nTL2idx = 0
+            if newTaskList1[nTL1idx]:
+                nTL1done = True
+            if newTaskList2[nTL2idx]:
+                nTL2done = True
+            if not cls.checkTaskInTList(taskList2[tL2idx],newTaskList1):
+                newTaskList1[nTL1idx] = taskList2[tL2idx]
+                nTL1idx += 1
+            if not cls.checkTaskInTList(taskList1[tL1idx],newTaskList2):
+                newTaskList2[nTL2idx] = taskList1[tL1idx]
+                nTL2idx += 1
+            tL1idx += 1
+            tL2idx += 1 #task list indices incremented either way
         #Return dict
-        newTaskDict = {}
-        for task in newTaskList:
-            newTaskDict[task] = taskDict1[task]
-        return newTaskDict
+        newTaskDict1, newTaskDict2 = {}, {}
+        for idx, taskTuple in enumerate(zip(newTaskList1,newTaskList2)):
+            task1, task2 = taskTuple
+            newTaskDict1[task1] = idx
+            newTaskDict2[task2] = idx
+        return newTaskDict1, newTaskDict2
+    '''
+    Args
+    - taskDict1: chrom. dictionary that maps tasks to its priority (Obj -> int)
+    - taskDict2: chrom. dictionary that maps tasks to its priority (Obj -> int)
+    - numPositions: optionally indicate the number of positions to generate
+    - positionList: optionally indicate a custom position list
+    Return
+    - New taskDict with the crossover of taskDict1 and taskDict2
+    '''
+    @classmethod 
+    def OX2(cls, taskDict1, taskDict2, numPositions=0, positionList=None):
+        ## Verification
+        if len(taskDict1) != len(taskDict2):
+            raise ValueError('Dictionary sizes don\'t match!')
+        positionList = sorted(list(set(positionList))) #cleanup duplicates/sort
+        if numPositions > len(taskDict1) or len(positionList) > len(taskDict1):
+            raise ValueError('Number of positions is greater than total items!')
+        for pos in positionList:
+            if pos >= len(taskDict1):
+                raise ValueError('Given invalid position!')
 
-    @staticmethod
-    def OX2(taskDict1, taskDict2, partition1=None, partition2= None):
-        pass #TODO
+        ## Get positions list
+        #cls.generateRandIdxList(0,numTasks,2)
 
+
+
+## ======================================================================== ##
+## Testing Crossovers
+## ======================================================================== ##
+
+### Testing Helper Functions
+'''
+Args
+- taskList: List of tasks where its index indicates its priority
+'''
+def printTaskList(taskList):
+    for idx,task in enumerate(taskList):
+        if task == None:
+            s = 'None'
+        else:
+            s = task.name
+        print 'Idx: %d: TL: %s' % (idx,s)
+    
+'''
+Emulator task object.
+'''
+class taskObj:
+    def __init__(self,name):
+        self.name = name
+
+###  Tests
+def testOX1():
+    TD1 = {taskObj('1'):0,taskObj('2'):1,taskObj('3'):2,taskObj('4'):3,
+            taskObj('5'):4,taskObj('6'):5,taskObj('7'):6,taskObj('8'):7}
+    TD2 = {taskObj('2'):0,taskObj('4'):1,taskObj('6'):2,taskObj('8'):3,
+            taskObj('7'):4,taskObj('5'):5,taskObj('3'):6,taskObj('1'):7,taskObj('10'):70}
+    newTD1, newTD2 = Crossovers.OX1(TD1,TD2,partition1=2,partition2=5)
+    print 'Test 1:'
+    for k,v in newTD1.items():
+        print 'Task: %s, Idx/Priority: %d' % (k.name,v)
+    print 'Test 2:'
+    for k,v in newTD2.items():
+        print 'Task: %s, Idx/Priority: %d' % (k.name,v)
+
+testOX1()
 
     
-        
+
         
--- Simulation.py
+++ Simulation.py
@@ -1,9 +1,41 @@
 import sys
 from simso.core import Model
 from simso.configuration import Configuration
-from GeneticAlgorithm import GeneticAlgorithm
 
+def getAverageNormalizedLaxity(model):
+
+    count = 0
+    totalNormalizedLaxity = 0
+    for task in model.results.tasks.values():
+        for job in task.jobs:
+            if(job.task.deadline and job.response_time):
+                totalNormalizedLaxity += job.normalized_laxity
+                count += 1
+
+    return totalNormalizedLaxity / count
             
+
+########### GA IMPLEMENTATION DETAILS BELOW ##############################
+
+def initial_population():
+
+
+def crossover():
+
+
+def mutate():
+
+
+def evaluate_fitness():
+
+
+def genetic_algorithm(model):
+
+
+
+
+########### GA IMPLEMENTATION DETAILS ABOVE ##############################
+
 def main(argv):
     if len(argv) == 2:
         # Configuration load from a file.
@@ -15,9 +47,14 @@ def main(argv):
     # Init a model from the configuration.
     model = Model(configuration)
 
-    geneticAlgorithm = GeneticAlgorithm(model.task_list)
+    # Execute the simulation.
+    model.run_model()
 
-    # geneticAlgorithm.genetic_algorithm(model)
 
+    print("Total Migrations: " + str(model.results.total_migrations))
+    print("Total Pre-emptions: " + str(model.results.total_preemptions))
+    print("Total Exceeded Count: " + str(model.results.total_exceeded_count))
+    print("Average Normalized Laxity: " +   "{:.3f}".format(getAverageNormalizedLaxity(model)))
 
+if 
 main(sys.argv)
