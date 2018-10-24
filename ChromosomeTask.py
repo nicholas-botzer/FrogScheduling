1b6ca98fee8204d8172d3a629616d4970d98986f
charley-zhang <yzhang46@nd.edu>
2018-10-24 15:41:58 -0400

    removed files

--- ChromosomeTask.py
+++
@@ -1,7 +0,0 @@
-class ChromosomeTask():
-
-    def __init__(self, name, deadline, activation_date, wcet):
-        self.name = name
-        self.deadline = deadline
-        self.activation_date = activation_date
-        self.wcet = wcet
