150f632dd4749517c64824637f3c938d248feb4e
charley-zhang <yzhang46@nd.edu>
2018-10-29 12:21:27 -0400

    trivial things

--- tmp.py
+++ tmp.py
@@ -1,6 +1,27 @@
 import argparse, sys, logging
 
-logging.basicConfig(level=logging.INFO)
 
-logging.debug('HELLO')
+x = 'glob_x'
+def outer():
+    x = 'outer_x'
+    def inner()
+        #x = 'iner_x'; print(x)
+        nonloca x; print(x)
+        global x; print(x)
+    inner()
+outer()
+sys.exit(0)
 
+class cls:
+    pass
+def method():
+    pass
+
+print(f'dir(cls)',dir(cls))
+print(f'dir(method)',dir(method))
+
+
+sys.exit(0)
+print(f'dir({__name__})',dir(__name__))
+
+print(f'globals()',globals())
--- tools/debug.py
+++ tools/debug.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 class Debug:
     
 ### Helper String/Print Statements
